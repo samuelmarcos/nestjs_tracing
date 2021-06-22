d988f2ed777803027d302a09069b91000316976a
samuelmarcos <samuelmarcos9580@gmail.com>
2021-06-21 23:38:38 -0300

    add user repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from '../database/database.module';
+import { userProviders } from './user.provider';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [...userProviders, UsersService]
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { CreateUser } from './usersDTO/createUserDTO';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@Inject('USER_REPOSITORY') private userRepository: Repository<User>) {}
+
+    async createUser(createUser: CreateUser): Promise<User> {
+        const { name, email, password }  = createUser;
+
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        await this.userRepository.save(user);
+        return user;
+    }
+}
